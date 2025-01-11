bf77e2f012951b8a0f9520ea2ec53f8171e6a056
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-01-11 17:01:51 +0200

    Implementation Remote Data Source Fetch Featured Product

--- lib/Feature/home/data/data_sources/home_remote_data_source.dart
+++ lib/Feature/home/data/data_sources/home_remote_data_source.dart
@@ -1,13 +1,27 @@
+import '../../../../Core/utils/api_service.dart';
 import '../../domain/Entities/product_entity.dart';
+import '../models/product_model.dart';
 
-abstract class HomeRemoteDataSource{
-  Future<List<ProductEntity>>fetchFeaturedProduct();
+abstract class HomeRemoteDataSource {
+  Future<List<ProductEntity>> fetchFeaturedProduct();
 }
 
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+  HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<ProductEntity>> fetchFeaturedProduct() {
-    // TODO: implement fetchFeaturedProduct
-    throw UnimplementedError();
+  Future<List<ProductEntity>> fetchFeaturedProduct() async {
+    var data = await apiService.get(endpoint: "products");
+    List<ProductEntity> products = getProductsList(data);
+    return products;
+  }
+
+  List<ProductEntity> getProductsList(Map<String, dynamic> data) {
+    List<ProductEntity> products = [];
+    for(var productMap in data[" "])
+      {
+        products.add(ProductModel.fromJson(productMap));
+      }
+    return products;
   }
 }
