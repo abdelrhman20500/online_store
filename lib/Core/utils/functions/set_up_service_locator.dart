26bc19011653107462d41b19db6fea95091b98a4
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-01-14 17:21:35 +0200

    Create Getit And Simple Bloc Observer

---
+++ lib/Core/utils/functions/set_up_service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import '../../../Feature/home/data/Repos/home_repo_impl.dart';
+import '../../../Feature/home/data/data_sources/home_local_data_source.dart';
+import '../../../Feature/home/data/data_sources/home_remote_data_source.dart';
+import '../api_service.dart';
+
+final getIt = GetIt.instance;
+
+void setUpServiceLocator(){
+  getIt.registerSingleton<ApiService>(ApiService(Dio()),);
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+      homeRemoteDataSource: HomeRemoteDataSourceImpl(
+        getIt.get<ApiService>(),
+      ),
+      homeLocalDataSource: HomeLocalDataSourceImpl()
+  ));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:online_store/Core/utils/functions/set_up_service_locator.dart';
+import 'package:online_store/Core/utils/functions/simple_bloc_observer.dart';
+import 'package:online_store/Feature/home/data/Repos/home_repo_impl.dart';
 import 'package:online_store/Feature/home/domain/Entities/product_entity.dart';
+import 'package:online_store/Feature/home/domain/use_cases/fetch_featured_product_use_case.dart';
 import 'package:online_store/Feature/home/presentation/view/home_view.dart';
+import 'package:online_store/Feature/home/presentation/view_manger/cubits/featured_products_cubit.dart';
 
 import 'constant/constant.dart';
 
 void main() async{
-  runApp(const MyApp());
   await Hive.initFlutter();
   Hive.registerAdapter(ProductEntityAdapter());
+  setUpServiceLocator();
   await Hive.openBox(kFeaturedBox);
+  Bloc.observer= SimpleBlocObserver();
+  runApp(const MyApp());
 }
 
+// final getIt = GetIt.instance;
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-     debugShowCheckedModeBanner: false,
-      home: HomeView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) {
+          return FeaturedProductsCubit(FetchFeaturedProductUseCase(
+             getIt.get<HomeRepoImpl>(),
+          ),
+          );
+        })
+      ],
+      child: const MaterialApp(
+       debugShowCheckedModeBanner: false,
+        home: HomeView(),
+      ),
     );
   }
 }
