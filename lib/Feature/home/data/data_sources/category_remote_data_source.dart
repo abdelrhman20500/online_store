35c627f618944d458ada8696a7ea548112d36b47
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-01-17 21:41:22 +0200

    Implement Category Remote Data Source

--- lib/Feature/home/data/data_sources/category_remote_data_source.dart
+++ lib/Feature/home/data/data_sources/category_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:online_store/Core/utils/api_service.dart';
+import 'package:online_store/Feature/home/data/models/category_name/category_name_model.dart';
 import 'package:online_store/Feature/home/domain/Entities/category_name_entity/category_name_entity.dart';
 
 abstract class CategoryRemoteDataSource {
@@ -5,9 +7,23 @@ abstract class CategoryRemoteDataSource {
 }
 
 class CategoryRemoteDataSourceImp extends CategoryRemoteDataSource{
+  final ApiService apiService;
+
+  CategoryRemoteDataSourceImp(this.apiService);
   @override
-  Future<List<CategoryNameEntity>> fetchCategoryByName(String categoryName) {
-    // TODO: implement fetchCategoryByName
-    throw UnimplementedError();
+  Future<List<CategoryNameEntity>> fetchCategoryByName(String categoryName) async{
+    var data = await apiService.get(endpoint: "products/category/$categoryName");
+    List<CategoryNameEntity> category = getCategoryList(data);
+    return category;
+  }
+
+  List<CategoryNameEntity> getCategoryList(List<dynamic> data) {
+    List<CategoryNameEntity> category= [];
+    for(var categoryMap in data)
+      {
+        category.add(CategoryNameModel.fromJson(categoryMap));
+      }
+    return category;
   }
 }
+
