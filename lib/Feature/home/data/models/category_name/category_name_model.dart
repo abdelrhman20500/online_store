efadef08069ec801586bddb80bce655f49e7999b
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-01-17 20:53:39 +0200

    Make Category Name Model

---
+++ lib/Feature/home/data/models/category_name/category_name_model.dart
@@ -0,0 +1,73 @@
+
+class CategoryNameModel {
+  int? id;
+  String? title;
+  int? price;
+  String? description;
+  String? category;
+  String? image;
+  Rating? rating;
+
+  CategoryNameModel({this.id, this.title, this.price, this.description, this.category, this.image, this.rating});
+
+  CategoryNameModel.fromJson(Map<String, dynamic> json) {
+    if(json["id"] is num) {
+      id = (json["id"] as num).toInt();
+    }
+    if(json["title"] is String) {
+      title = json["title"];
+    }
+    if(json["price"] is num) {
+      price = (json["price"] as num).toInt();
+    }
+    if(json["description"] is String) {
+      description = json["description"];
+    }
+    if(json["category"] is String) {
+      category = json["category"];
+    }
+    if(json["image"] is String) {
+      image = json["image"];
+    }
+    if(json["rating"] is Map) {
+      rating = json["rating"] == null ? null : Rating.fromJson(json["rating"]);
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data["id"] = id;
+    data["title"] = title;
+    data["price"] = price;
+    data["description"] = description;
+    data["category"] = category;
+    data["image"] = image;
+    if(rating != null) {
+      data["rating"] = rating?.toJson();
+    }
+    return data;
+  }
+}
+
+class Rating {
+  double? rate;
+  int? count;
+
+  Rating({this.rate, this.count});
+
+  Rating.fromJson(Map<String, dynamic> json) {
+    if(json["rate"] is num) {
+      rate = (json["rate"] as num).toDouble();
+    }
+    if(json["count"] is num) {
+      count = (json["count"] as num).toInt();
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data["rate"] = rate;
+    data["count"] = count;
+    return data;
+  }
+}
